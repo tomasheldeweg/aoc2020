fd4fa54557b5da745f415f68e3ac499be23987a4
Tomas Heldeweg <tomasheldeweg@gmail.com>
2020-12-02 23:13:29 +0100

    Add solution day 2

--- day2/solution.py
+++ day2/solution.py
@@ -1,13 +1,15 @@
-"""
-No mem solve with chained generators
-"""
-def find_number_pair(filename, total=2020):
-    with open(filename) as f:
-        for i, line in enumerate(f):
-            with open(filename) as f2:
-                for j, line2 in enumerate(f2):
-                    with open(filename) as f3:
-                        for k, line3 in enumerate(f3):
-                            if int(line) + int(line2) + int(line3) == total and len(set([i, j, k])) == 3:
-                                return int(line) * int(line2) * int(line3)
-print(find_number_pair('day2/input.txt'))
+import re
+def get_valid_entries(filename, method='charcount'):
+    policy_pw_generator = (re.split('-|:\s|\s', line.rstrip('\n')) for line in open(filename))
+    count = 0
+    for _min, _max, char, pw in policy_pw_generator:
+        _min, _max = int(_min), int(_max)
+        if method == 'charcount' and _min <= pw.count(char) <= _max:
+            count += 1
+        if method == 'position' and (bool(pw[_min - 1: _min] == char) ^ bool(pw[_max - 1: _max] == char)):
+            count += 1
+    return count
+
+print(get_valid_entries('day2/input.txt'))
+
+print(get_valid_entries('day2/input.txt', method='position'))
